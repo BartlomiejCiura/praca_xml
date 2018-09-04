b362b4466fdfb2cb9289dcb75c3e54bf28aa8085
BartlomiejCiura <bartlomiej.ciura@gmail.com>
2018-09-04 08:16:46 +0200

    wersja robocza

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,14 +11,15 @@ namespace ConsoleApp1
 
         class Product
         {
-            private string ItemDescription; // opis
-            private double InvoiceQuantity; // ilość opakowań
-            private double InvoiceUnitPacksize; // ilość sztuk w opakowaniu
-            private double InvoiceUnitAllSingleNetPrice; // ilość sztuk * cena za sztukę
-            private double InvoiceUnitNetPrice; // cena za opakowanie
-            private double TaxRate; // podatek
-            private double InvoiceSingleNetPrice; // cena za sztukę
-            private double Difference;
+            public string ItemDescription; // opis
+            public double InvoiceQuantity; // ilość opakowań
+            public double InvoiceSingleQuantity;
+            public double InvoiceUnitPacksize; // ilość sztuk w opakowaniu
+            public double InvoiceUnitAllSingleNetPrice; // ilość sztuk * cena za sztukę
+            public double InvoiceUnitNetPrice; // cena za opakowanie
+            public double TaxRate; // podatek
+            public double InvoiceSingleNetPrice; // cena za sztukę
+            public double Difference;
 
             public Product(string itemDescription, string invoiceQuantity, string invoiceUnitPacksize, string invoiceUnitNetPrice, string taxRate)
             {
@@ -28,10 +29,27 @@ namespace ConsoleApp1
                 InvoiceUnitNetPrice = Convert.ToDouble(invoiceUnitNetPrice);
                 TaxRate = Convert.ToDouble(taxRate);
 
-                InvoiceSingleNetPrice = Math.Round(InvoiceUnitNetPrice / InvoiceUnitPacksize, 2);
+                InvoiceSingleNetPrice = (Math.Floor((InvoiceUnitNetPrice / InvoiceUnitPacksize) * 100)) / 100;
                 InvoiceUnitAllSingleNetPrice = InvoiceSingleNetPrice * InvoiceQuantity * InvoiceUnitPacksize;
-                Difference = Math.Round(((InvoiceQuantity * InvoiceUnitNetPrice)-InvoiceUnitAllSingleNetPrice), 2);
+            //    Difference = Math.Round(((InvoiceQuantity * InvoiceUnitNetPrice)-InvoiceUnitAllSingleNetPrice), 2);
+                // InvoiceUnitAllSingleNetPrice += Difference;
+                //InvoiceSingleNetPrice += Difference / InvoiceUnitPacksize;
+
+               // InvoiceUnitAllSingleNetPrice = InvoiceSingleNetPrice * InvoiceQuantity * InvoiceUnitPacksize;
+
                 Console.WriteLine(Difference);
+            }
+
+            public Product(string itemDescription, double invoiceQuantity, double invoiceUnitPacksize, double invoiceUnitNetPrice, double taxRate)
+            {
+                ItemDescription = itemDescription;
+                InvoiceQuantity = invoiceQuantity;
+                InvoiceUnitPacksize = invoiceUnitPacksize;
+                InvoiceUnitNetPrice = invoiceUnitNetPrice;
+                TaxRate = taxRate;
+
+                InvoiceSingleNetPrice = (Math.Floor((InvoiceUnitNetPrice / InvoiceUnitPacksize) * 100)) / 100;
+                InvoiceUnitAllSingleNetPrice = InvoiceSingleNetPrice * InvoiceQuantity * InvoiceUnitPacksize;
             }
 
             public double GetInvoiceSingleNetPrice() => InvoiceSingleNetPrice;
@@ -140,14 +158,45 @@ namespace ConsoleApp1
                 productsList.Add(new Product(r.ItemDescription, r.InvoiceQuantity, r.InvoiceUnitPacksize, r.InvoiceUnitNetPrice, r.TaxRate));
             }
 
-            foreach (var item in productsList)
+            List<Product> wynik = new List<Product>();
+            foreach (Product item in productsList)
             {
+                List<Product> splitedProduct = CalculatePrice(item);
+                wynik.AddRange(splitedProduct);
                 Console.WriteLine(item.ToString());
             }
 
-            double total = CalculateTotal(productsList);
-            Console.WriteLine("Total: " + total);
 
+
+            double total = 0;// CalculateTotal(productsList);
+            foreach (Product item in wynik)
+            {
+                total += item.InvoiceSingleQuantity * item.InvoiceSingleNetPrice;
+            }
+
+                Console.WriteLine("Total: " + total);
+
+        }
+
+        private static List<Product> CalculatePrice(Product item)
+        {
+            double cena = item.GetInvoiceSingleNetPrice();
+            double wartoscZaokr = item.InvoiceQuantity * item.InvoiceUnitPacksize * cena;
+            double wartReal = item.InvoiceQuantity * item.InvoiceUnitNetPrice;
+            double r = Math.Abs( wartoscZaokr - wartReal);
+            int ilosc = (int)Math.Round(r * 100); 
+
+            Product p1 = new Product(item.ItemDescription, item.InvoiceQuantity, item.InvoiceUnitPacksize, item.InvoiceUnitNetPrice, item.TaxRate);
+            p1.InvoiceSingleQuantity = ilosc;
+            p1.InvoiceSingleNetPrice += 0.01;
+
+            Product p2 = item;
+            p2.InvoiceSingleQuantity = (int)(item.InvoiceQuantity * item.InvoiceUnitPacksize) - ilosc;
+            List<Product> ret = new List<Product>();
+            ret.Add(p1);
+            ret.Add(p2);
+
+            return ret;
         }
 
         private static XDocument LoadDocument()
